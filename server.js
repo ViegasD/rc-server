9ce97d20785ec3e5988bc120b005226c09f7be39
ViegasD <fabioavsouza@outlook.com>
2025-03-11 11:08:53 -0300

    Atualização no server.js - adicionando port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const routeros = require('node-routeros');
+const RouterOSClient = require('node-routeros');
 const express = require('express');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid'); // Para gerar UUIDs
